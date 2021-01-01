4b6338d890460fd667ba05063fc996596c4723ba
Alex Barker <abarke28@gmail.com>
2021-01-01 14:23:43 -0800

    add scheduled task triggers

--- scripts/generate_task.ps1
+++ scripts/generate_task.ps1
@@ -1,3 +1,16 @@
-﻿# Script to generate windows scheduled task to run poller
+﻿# -----------------------------------------------------------
+# Script: generate_task.ps1
+# Author: Alex Barker
+# Date: 2021-01-01
+# Description: Generate windows scheduled task to run poller
+# -----------------------------------------------------------
 
 Write-Output "Generating Windows Scheduled Task for Poller"
+
+$action = New-ScheduledTaskAction -Execute 'discover-camping.exe'
+
+$triggers = @()
+$triggers += New-ScheduledTaskTrigger -Daily -At 06:00 
+$triggers += New-ScheduledTaskTrigger -Daily -At 06:01 
+$triggers += New-ScheduledTaskTrigger -Daily -At 07:00
+$triggers += New-ScheduledTaskTrigger -Daily -At 07:01 
