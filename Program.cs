bfcfcf4a354048cad6b9b3effdf42be591c69cca
Alex Barker <abarke28@gmail.com>
2021-01-01 08:49:52 -0800

    initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace discover_camping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
