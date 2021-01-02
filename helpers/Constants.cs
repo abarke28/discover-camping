051e1f0d727543c6c619e5f3d2906004e918ddd1
Alex Barker <abarke28@gmail.com>
2021-01-02 15:15:58 -0800

    fix folder casing

---
+++ helpers/Constants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace discover_camping.helpers
+{
+    public static class Constants
+    {
+        public const string HEADLESS = "headless";
+
+        public const string DISCOVER_HOMEPAGE = "https://www.discovercamping.ca/BCCWeb/Default.aspx";
+        public const string DISCOVER_BACKCOUNTRY_RESERVATIONS = "https://www.discovercamping.ca/BCCWeb/Facilities/TrailRiverCampingSearchView.aspx";
+
+        public const string NOT_AVAILABLE = "Not Available";
+
+        public const int AUGUST = 8;
+        public const string SIX = "6";
+
+        public const string SENDER_EMAIL = "SenderEmail";
+        public const string SENDER_PW = "SenderPw";
+        public const string EMAILS = "Emails";
+        public const string SUBJECT = "Discover Camping Alert";
+        public const string BODY = "Discover Camping Reservations are now available online at:\n {0}";
+
+        public const string GMAIL = "smtp.gmail.com";
+        public const int PORT = 587;
+
+        public const string LOG_FILE = @"c:\Users\Public\Logs\poller.log";
+    }
+}
---
+++ helpers/XPath.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace discover_camping.helpers
+{
+    public static class XPath
+    {
+        public const string BergLake = "//option[. = 'Berg Lake Trail']";
+    }
+}
